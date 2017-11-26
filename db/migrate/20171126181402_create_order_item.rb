a477a36ee051d92703bbed3b2ebc54633078763b
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-26 20:10:58 +0100

    Revert to base CreateOrderItem

--- db/migrate/20171126181402_create_order_item.rb
+++ db/migrate/20171126181402_create_order_item.rb
@@ -1,6 +1,4 @@
 class CreateOrderItem < ActiveRecord::Migration[4.2]
   def change
-    add_reference :it.ems_order :product foreign_key: true
-    add_reference :items_order :order foreign_key: true
   end
 end
