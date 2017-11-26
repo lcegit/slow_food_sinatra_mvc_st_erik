cb9ebf0b096b448650c5287b6734ba2c2f1ab68e
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-11-26 21:06:06 +0100

    Add order controller – post '/order_create'

---
+++ app/controllers/order_controller.rb
@@ -0,0 +1,13 @@
+class SlowFoodApp
+  post '/order_create' do
+    product = Product.find(params['product-id'])
+    if session[:order_id]
+      order = Order.find(session[:order_id])
+    else
+      order = Order.create
+      session[:order_id] = order.id
+    end
+    order_item = OrderItem(product: product, order: order)
+    redirect '/', notice: "#{order_item.product.name} has been added to your order"
+  end
+end
